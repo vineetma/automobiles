e8c086f4d8cb322e36e8232259dad6d059cd94ed
gaurav <gauravyadav32@gmail.com>
2014-02-06 14:43:30 +0530

    added

--- src/com/dkt/Automobile/Bus.java
+++ src/com/dkt/Automobile/Bus.java
@@ -1,6 +1,6 @@
 package com.dkt.Automobile;
 
-public class Bus extends FourWheeler  {
+public class Bus extends FourWheeler implements java.io.Serializable  {
 	public Bus(int a,int p,int e){
 		super(a,p,e);
 		
